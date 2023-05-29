c19fd4fd5db5807294b80e45f360020352f492e4
HaeTi <t9h9@naver.com>
2023-05-29 14:59:25 +0900

    splash 추가

--- app/src/main/java/sopt/haeti/nyussu/MainActivity.kt
+++ app/src/main/java/sopt/haeti/nyussu/MainActivity.kt
@@ -2,10 +2,13 @@ package sopt.haeti.nyussu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil.setContentView
+import sopt.haeti.nyussu.databinding.ActivityMainBinding
+import sopt.haeti.nyussu.util.BindingActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
